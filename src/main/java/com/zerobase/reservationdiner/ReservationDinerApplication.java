70aee2beadf0b9e670f494a0251c69f883eb7319
haebeen <haebing0309@gmail.com>
2023-10-24 16:41:35 +0900

    [Member] member entity,controller,dto 설정 완료

--- src/main/java/com/zerobase/reservationdiner/ReservationDinerApplication.java
+++ src/main/java/com/zerobase/reservationdiner/ReservationDinerApplication.java
@@ -1,4 +1,4 @@
-package com.zerobase.ReservationDiner;
+package com.zerobase.reservationdiner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/zerobase/reservationdiner/ReservationDinerApplicationTests.java
+++ src/test/java/com/zerobase/reservationdiner/ReservationDinerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zerobase.ReservationDiner;
+package com.zerobase.reservationdiner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
