8eaac5cc21d377fd7f2206ddd1c054143ecdfcb0
haebeen <haebing0309@gmail.com>
2023-10-24 15:43:39 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/ReservationDiner/ReservationDinerApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.ReservationDiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationDinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationDinerApplication.class, args);
+	}
+
+}
